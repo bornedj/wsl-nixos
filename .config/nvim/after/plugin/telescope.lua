b9cbf15a6c1c8ebaacb41cae62960157a9dfbfdb
daniel.borne <daniel.borne@kinsaleins.com>
2024-02-19 09:28:10 -0500

    added nvim config files

---
+++ .config/nvim/after/plugin/telescope.lua
@@ -0,0 +1,14 @@
+require("telescope").setup {
+    defaults = {
+        path_display = { "truncate" },
+    }
+}
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fh', '<cmd>Telescope find_files hidden=true<CR>')
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fs', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
