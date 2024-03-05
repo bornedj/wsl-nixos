360e0646476a821b6844cdd09f6b8afa6ee843ee
daniel.borne <daniel.borne@kinsaleins.com>
2024-03-05 13:12:37 -0500

    chore: removing redundant color configuration

--- .config/nvim/lua/daniel/set.lua
+++ .config/nvim/lua/daniel/set.lua
@@ -32,14 +32,3 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 -- vim.opt.colorcolumn = "80"
-vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-vim.api.nvim_set_hl(0, "CocInlayHint", {bg = "none"})
-vim.api.nvim_set_hl(0, 'TelescopeNormal', {bg= "none"})
-vim.api.nvim_set_hl(0, 'TelescopeBackground', {bg= "none"})
-vim.api.nvim_set_hl(0, 'NormalFloat', {bg= "none"})
-vim.api.nvim_set_hl(0, 'FloatBorder', {bg= "none"})
-vim.api.nvim_set_hl(0, 'LspDiagnosticsDefaultError', {bg= "none"})
-vim.api.nvim_set_hl(0, 'LspDiagnosticsDefaultWarning', {bg= "none"})
-vim.api.nvim_set_hl(0, 'LspDiagnosticsDefaultInformation', {bg= "none"})
-vim.api.nvim_set_hl(0, 'LspDiagnosticsDefaultHint', {bg= "none"})
-vim.api.nvim_set_hl(0, 'LspDiagnosticsUnderlineWarning', {bg= "none"})
