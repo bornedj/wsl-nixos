53c11dfcc0e7018da712470bfcedd94c37c302a6
daniel.borne <daniel.borne@kinsaleins.com>
2024-03-28 09:31:17 -0400

    fix: fixing path for tpm

--- .local/bin/tmux-sessionizer.sh
+++ .local/bin/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/gitlab_repos ~/obsidian ~/dotfiles -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/gitlab_linux ~/obsidian ~/dotfiles -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
