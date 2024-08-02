98c6d4e2fb331f438226c068b7b2a3df855257de
daniel.borne <daniel.borne@kinsaleins.com>
2024-08-02 13:02:37 -0400

    feat: adding shortcut to grep commits

--- .config/nvim/lua/daniel/plugins/telescope.lua
+++ .config/nvim/lua/daniel/plugins/telescope.lua
@@ -23,6 +23,8 @@ return {
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fh', '<cmd>Telescope find_files hidden=true<CR>')
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        -- grep commits
+        vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
         vim.keymap.set('n', '<leader>fs', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
