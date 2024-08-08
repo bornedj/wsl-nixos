4cd7c9cb62814e9bc0783021a8f81f897b918822
daniel.borne <daniel.borne@kinsaleins.com>
2024-08-08 20:01:05 +0000

    fix: fixing plenary naming issue

--- .config/nvim/lua/daniel/plugins/init.lua
+++ .config/nvim/lua/daniel/plugins/init.lua
@@ -1,8 +1,5 @@
 return {
-    {
-        "nvim-lua/plenary.nvim",
-        name = "plenary",
-    },
+    "nvim-lua/plenary.nvim",
     {
         "folke/trouble.nvim",
         config = function ()
