fa9ac077e9acb522b1c86d4f589b6352c7a9a617
borne.danielj <borne.danielj@gmail.com>
2024-08-09 01:36:50 +0000

    feat: adding etc nixos config files

---
+++ etc/nixos/home.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+    home.username = "nixos";
+    home.homeDirectory = "/home/nixos";
+
+    home.packages = with pkgs; [
+        neovim
+        git
+        rustup
+        python3
+        nodejs
+        lua
+        tmux
+        zoxide
+        fzf
+        stow
+        curl
+    ];
+
+    home.stateVersion =  "24.05";
+
+    programs.home-manager.enable = true;
+}
