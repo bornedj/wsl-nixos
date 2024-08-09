d9e2f4e91e9560b0a883f7f77cb3bedde737ae0a
borne.danielj <borne.danielj@gmail.com>
2024-08-09 02:22:02 +0000

    fix: fixing tmux sessionizer env

--- .local/bin/tmux-sessionizer.sh
+++ .local/bin/tmux-sessionizer.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/gitlab_linux ~/obsidian ~/dotfiles -mindepth 1 -maxdepth 1 -type d | fzf)
+    # selected=$(find ~/gitlab_linux ~/obsidian ~/dotfiles -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/gitlab_linux -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
