956e854e47fa288ade2739b3a4842b32d81b3d09
daniel.borne <daniel.borne@kinsaleins.com>
2024-08-09 16:04:58 +0000

    feat: wip nixpkg for ggshield whl

---
+++ etc/nixos/pkgs/ggshield.nix
@@ -0,0 +1,63 @@
+{
+  lib,
+  fetchFromGitHub,
+  git,
+  python3,
+}:
+
+python3.pkgs.buildPythonApplication rec {
+  pname = "ggshield";
+  version = "1.30.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "GitGuardian";
+    repo = "ggshield";
+    rev = "v1.30.2";
+    sha256 = "gN8dnUIwK/g8BgbwEdkoKihNZUrNWrNm5CCIbqZrQgY=";
+  };
+
+  pythonRelaxDeps = true;
+
+  build-system = with python3.pkgs; [ setuptools ];
+
+  dependencies = with python3.pkgs; [
+    appdirs
+    charset-normalizer
+    click
+    cryptography
+    marshmallow
+    marshmallow-dataclass
+    oauthlib
+    platformdirs
+    pygitguardian
+    pyjwt
+    python-dotenv
+    pyyaml
+    requests
+    rich
+  ];
+
+  nativeCheckInputs =
+    [ git ]
+    ++ (with python3.pkgs; [
+      jsonschema
+      pyfakefs
+      pytest-mock
+      pytest-voluptuous
+      pytestCheckHook
+      snapshottest
+      vcrpy
+    ]);
+
+  pythonImportsCheck = [ "ggshield" ];
+
+  meta = with lib; {
+    description = "Tool to find and fix various types of hardcoded secrets and infrastructure-as-code misconfigurations";
+    homepage = "https://github.com/GitGuardian/ggshield";
+    changelog = "https://github.com/GitGuardian/ggshield/blob/${version}/CHANGELOG.md";
+    license = licenses.mit;
+    maintainers = with maintainers; [ fab ];
+    mainProgram = "ggshield";
+  };
+}
