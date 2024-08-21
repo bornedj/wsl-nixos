3c0ae1a5cd94934775bbb3c5d93a305b32fd090c
daniel.borne <daniel.borne@kinsaleins.com>
2024-08-21 16:44:02 +0000

    fix: updating java configuration for new nixos paths

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -3,22 +3,7 @@ local root_markers = { 'gradlew', '.git', 'mvnw', 'pom' }
 
 local config = {
     cmd = {
-        '/usr/lib/jdtls/bin/jdtls',
-        '/usr/lib/jvm/java-1.17.0-openjdk-amd64/bin/java',
-        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-        "-Dosgi.bundles.defaultStartLevel=4",
-        "-Declipse.product=org.eclipse.jdt.ls.core.product",
-        "-Dlog.protocol=true",
-        "-Dlog.level=ALL",
-        "-Xmx1g",
-        "--add-modules=ALL-SYSTEM",
-        "--add-opens",
-        "java.base/java.util=ALL-UNNAMED",
-        "--add-opens",
-        "java.base/java.lang=ALL-UNNAMED",
-        '-configuration', '/usr/lib/jdtls/config_linux',
-        '-jar', vim.fn.glob('/usr/lib/jdtls/plugins/org.eclipse.equinox.launcher_*.jar*'),
-        '-data', workspace_dir
+        '/etc/profiles/per-user/nixos/bin/jdtls'
     },
 
     root_dir = require('jdtls.setup').find_root(root_markers),
@@ -28,12 +13,8 @@ local config = {
             configuration = {
                 runtimes = {
                     {
-                        name = "JavaSE-1.8",
-                        path = "/usr/lib/jvm/java-8-openjdk-amd64/"
-                    },
-                    {
-                        name = "JavaSE-17",
-                        path = "/usr/lib/jvm/java-17-openjdk-amd64/"
+                        name = "JavaSE-21",
+                        path = "/etc/profiles/per-user/nixos/lib/openjdk"
                     }
                 }
             },
