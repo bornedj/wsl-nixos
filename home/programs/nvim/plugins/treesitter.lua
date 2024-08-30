36a8766052c8be36a559cb443e7c99e30c6040bf
daniel.borne <daniel.borne@kinsaleins.com>
2024-08-30 12:42:34 +0000

    fix: treesitter managed by nix config

--- home/programs/nvim/plugins/treesitter.lua
+++ home/programs/nvim/plugins/treesitter.lua
@@ -1,30 +1,23 @@
-return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    lazy = false,
-    config = function ()
-        require'nvim-treesitter.configs'.setup {
-            ensured_installed = { "help", "javascript", "typescript", "rust", "c", "lua", "vimdoc"},
+require'nvim-treesitter.configs'.setup {
+    ensured_installed = { "help", "javascript", "typescript", "rust", "c", "lua", "vimdoc"},
 
-            sync_install = false,
+    sync_install = false,
 
-            auto_install = true,
+    auto_install = true,
 
-            highlight = {
-                enable = true,
+    highlight = {
+        enable = true,
 
-                additional_vim_regex_highlighting = false,
-            },
-        }
-        local ft_to_parser = require("nvim-treesitter.parsers")
-        ft_to_parser.mdx = "markdown"
-        ft_to_parser.sh = "bash"
-        ft_to_parser.java = "java"
-
-        vim.filetype.add({
-            extension = {
-                mdx = 'jsx'
-            }
-        })
-    end,
+        additional_vim_regex_highlighting = false,
+    },
 }
+local ft_to_parser = require("nvim-treesitter.parsers")
+ft_to_parser.mdx = "markdown"
+ft_to_parser.sh = "bash"
+ft_to_parser.java = "java"
+
+vim.filetype.add({
+    extension = {
+        mdx = 'jsx'
+    }
+})
