2b4c10891c041309f0375737e5b7b84f74424bd0
daniel.borne <daniel.borne@kinsaleins.com>
2024-08-30 13:12:29 +0000

    fix: remove deprecated setting

--- home/programs/nvim/plugins/lsp.lua
+++ home/programs/nvim/plugins/lsp.lua
@@ -30,10 +30,6 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-Space>'] = cmp.mapping.complete(),
 })
 
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
-})
-
 require('mason').setup({})
 require('mason-lspconfig').setup({
     ensure_installed = {
