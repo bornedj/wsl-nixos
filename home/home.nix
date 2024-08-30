8b630a5a5f1c3334b68743eb5e07c21adf12efee
daniel.borne <daniel.borne@kinsaleins.com>
2024-08-30 14:48:19 +0000

    fix: managing parsers with treesitter
    There's an open issue around the fact that installing parsers via nix
    leads to read only file system errors

--- home/home.nix
+++ home/home.nix
@@ -84,6 +84,11 @@ in
         '';
 
         plugins = with pkgs.vimPlugins; [
+            {
+                # managing parsers itself, there's an open issue around doing it with nix
+                plugin = nvim-treesitter;
+                config = toLuaFile ./programs/nvim/plugins/treesitter.lua;
+            }
             plenary-nvim
             trouble-nvim
             {
@@ -144,21 +149,6 @@ in
             {
                 plugin = which-key-nvim;
                 config = toLuaFile ./programs/nvim/plugins/which-key.lua;
-            }
-            {
-                plugin = (nvim-treesitter.withPlugins (p: [
-                    p.tree-sitter-java
-                    p.tree-sitter-typescript
-                    p.tree-sitter-rust
-                    p.tree-sitter-lua
-                    p.tree-sitter-bash
-                    p.tree-sitter-javascript
-                    p.tree-sitter-nix
-                    p.tree-sitter-c
-                    p.tree-sitter-vim
-                    p.tree-sitter-python
-                ]));
-                config = toLuaFile ./programs/nvim/plugins/treesitter.lua;
             }
         ];
     };
--- home/programs/nvim/plugins/treesitter.lua
+++ home/programs/nvim/plugins/treesitter.lua
@@ -1,16 +1,20 @@
-require'nvim-treesitter.configs'.setup {
+-- current workaround to install grammers with nix
+-- Defines a read-write directory for treesitters in nvim's cache dir
+local parser_install_dir = vim.fn.stdpath("cache") .. "/treesitters"
+vim.fn.mkdir(parser_install_dir, "p")
+-- Prevents reinstall of treesitter plugins every boot
+vim.opt.runtimepath:append(parser_install_dir)
+
+require('nvim-treesitter.configs').setup({
     ensure_installed = {},
-
     sync_install = false,
-
     auto_install = true,
-
     highlight = {
         enable = true,
-
         additional_vim_regex_highlighting = false,
     },
-}
+    parser_install_dir = parser_install_dir,
+})
 local ft_to_parser = require("nvim-treesitter.parsers")
 ft_to_parser.mdx = "markdown"
 ft_to_parser.sh = "bash"
