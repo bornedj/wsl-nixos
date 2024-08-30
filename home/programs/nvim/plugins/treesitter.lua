5f1e7289d98f7719eb7a5be4e53d8bc56d7a0b81
daniel.borne <daniel.borne@kinsaleins.com>
2024-08-30 13:51:50 +0000

    fix: removing ensured installed

--- home/programs/nvim/plugins/treesitter.lua
+++ home/programs/nvim/plugins/treesitter.lua
@@ -1,6 +1,4 @@
 require'nvim-treesitter.configs'.setup {
-    ensured_installed = { "help", "javascript", "typescript", "rust", "c", "lua", "vimdoc"},
-
     sync_install = false,
 
     auto_install = true,
