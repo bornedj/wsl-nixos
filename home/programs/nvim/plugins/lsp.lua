66b55ead86ad324645aabbfcab7383e2f076e705
daniel.borne <daniel.borne@kinsaleins.com>
2024-09-19 20:14:25 +0000

    fix: remapping lsp keymappings

--- home/programs/nvim/plugins/lsp.lua
+++ home/programs/nvim/plugins/lsp.lua
@@ -1,7 +1,7 @@
 local lsp = require('lsp-zero')
 
-local lsp_attach = function(client, buffer)
-    local opts = { buffer = bufnr, remap = false }
+local lsp_attach = function(_, bufnr)
+    local opts = { buffer = bufnr }
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "gi", function() vim.lsp.buf.declaration() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
