27e08351d9eeb859965e8bece286ba4ab507d2fd
daniel.borne <daniel.borne@kinsaleins.com>
2024-09-24 15:19:36 +0000

    fix: path for update alias

--- home/users/work-wsl/programs/zsh.nix
+++ home/users/work-wsl/programs/zsh.nix
@@ -28,7 +28,7 @@
             gen_dms_jwt="docker run --rm --name jwt-cli bitnami/jwt-cli:latest encode -S b64:$JWT -P 'auth=[\"ROLE_ADMIN\"]' -s anonymous -A HS512 --no-typ -e=$(date -d '+1 days' +%s) | clip.exe";
             gen_doc="rm -rf docs/ && npm run doc";
 
-            update = "cd ~/config && sudo nixos-rebuild switch --flake .#nixos";
+            update = "cd ~/dotfiles && sudo nixos-rebuild switch --flake .#nixos";
         };
 
         profileExtra = ''
