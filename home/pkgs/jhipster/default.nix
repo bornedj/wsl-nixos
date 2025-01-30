8825308fe734bfd8be688ded23092d098558a0b3
daniel.borne <daniel.borne@kinsaleins.com>
2025-01-30 18:10:34 +0000

    feat: package jhipster

---
+++ home/pkgs/jhipster/default.nix
@@ -0,0 +1,11 @@
+let
+  nixpkgs = fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+      sha256 = "0zydsqiaz8qi4zd63zsb2gij2p614cgkcaisnk11wjy3nmiq0x1s";
+  };
+  pkgs = import nixpkgs { config = {}; overlays = []; system = "x86_64-linux"; };
+in
+{
+  jhipster = pkgs.callPackage ./jhipster.nix {};
+}
+
