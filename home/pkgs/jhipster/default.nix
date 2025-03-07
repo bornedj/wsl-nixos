360c16853da183d396e06de15f01ab521531a86c
daniel.borne <daniel.borne@kinsaleins.com>
2025-03-07 01:29:12 +0000

    chore: sha update

--- home/pkgs/jhipster/default.nix
+++ home/pkgs/jhipster/default.nix
@@ -1,7 +1,7 @@
 let
   nixpkgs = fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
-      sha256 = "1wr1xzkw7v8biqqjcr867gbpqf3kibkgly36lcnhw0glvkr1i986";
+      sha256 = "0h3yzgn0mw74039xaqpvhvd2f924d923ax3kb8gh79f2m1jgla6i";
   };
   pkgs = import nixpkgs { config = {}; overlays = []; system = "x86_64-linux"; };
 in
