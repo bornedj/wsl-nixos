5ad2fd853faec30fce42f9754afca1075dfc208a
daniel.borne <daniel.borne@kinsaleins.com>
2025-03-31 12:31:10 +0000

    chore: has mismatch

--- home/pkgs/ggshield/default.nix
+++ home/pkgs/ggshield/default.nix
@@ -2,7 +2,7 @@
 let
   nixpkgs = fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
-      sha256 = "0h3yzgn0mw74039xaqpvhvd2f924d923ax3kb8gh79f2m1jgla6i";
+      sha256 = "08pwvljh8qskw8ajggfpx4x2jc1x69jc9vqkqzx68aj66vb2rsmh";
   };
   pkgs = import nixpkgs { config = {}; overlays = []; system = "x86_64-linux"; };
 
