2f203f849460f143d30a98c2c7634a0c5f1e4804
daniel.borne <daniel.borne@kinsaleins.com>
2025-04-25 14:52:20 +0000

    chore: sha updates

--- home/pkgs/ggshield/default.nix
+++ home/pkgs/ggshield/default.nix
@@ -2,7 +2,7 @@
 let
   nixpkgs = fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
-      sha256 = "0npc13dr4yzk6n4crkpjq6lcdw2zw1j70sdfj2zzdz6s0kvwq3h1";
+      sha256 = "1p3mbqpj4nhq88vnfcajq8yw7mp6bf7s63n10a0hg38d3pfj42hm";
   };
   pkgs = import nixpkgs { config = {}; overlays = []; system = "x86_64-linux"; };
 
