817c3bb8cdca05d336631c3a2c806080a59efe2b
Daniel Borne <daniel.borne@kinsaleins.com>
2025-06-26 18:25:58 +0000

    chore: don't enter claude in insert mode

--- home/users/work-wsl/programs/nvim/plugins/claude-code.lua
+++ home/users/work-wsl/programs/nvim/plugins/claude-code.lua
@@ -2,7 +2,8 @@ require("claude-code").setup({
     window = {
         position = "vertical",
         split_ratio = 0.5,
-    }
+        enter_insert = false,
+    },
 })
 
 vim.keymap.set("n", "<leader>cc", '<cmd>ClaudeCode<cr>', {
