ed2612e67132f9cb05a7061d53e5a5a94f036199
Daniel Borne <daniel.borne@kinsaleins.com>
2025-06-26 13:52:41 +0000

    refactor: change some claude settings

--- home/users/work-wsl/programs/nvim/plugins/claude-code.lua
+++ home/users/work-wsl/programs/nvim/plugins/claude-code.lua
@@ -1,1 +1,10 @@
-require("claude-code").setup()
+require("claude-code").setup({
+    window = {
+        position = "vertical",
+        split_ratio = 0.5,
+    }
+})
+
+vim.keymap.set("n", "<leader>cc", '<cmd>ClaudeCode<cr>', {
+    desc = "Toggle Claude Code",
+})
