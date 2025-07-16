17944a3afc001c8b8042d89ab84bdc50a826a368
Daniel Borne <daniel.borne@kinsaleins.com>
2025-07-16 12:43:05 +0000

    build: sync nixpkgs and home manager input versions

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix";
   };
