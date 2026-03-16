40e842b945e24f92d46585c5ef6dc97bfa5247c0
Daniel Borne <daniel.borne@kinsaleins.com>
2026-03-16 11:39:22 -0400

    chore: remove redundant config option

--- home/users/work-wsl/programs/nvim/plugins/lsp.lua
+++ home/users/work-wsl/programs/nvim/plugins/lsp.lua
@@ -31,9 +31,6 @@ vim.lsp.enable('ts_ls', {})
 -- bug with eslint running in html
 vim.lsp.config('eslint', {
     filetypes = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
-    settings = {
-        filetypes = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
-    }
 })
 vim.lsp.enable('eslint')
 vim.lsp.enable('rust_analyzer', {
