9c58ee94bbb93b37067366c1a6bfc312f8d7cb60
Daniel Borne <daniel.borne@kinsaleins.com>
2026-03-24 11:17:54 -0400

    style: run lua formatter

--- home/users/work-wsl/programs/nvim/plugins/lsp.lua
+++ home/users/work-wsl/programs/nvim/plugins/lsp.lua
@@ -1,9 +1,9 @@
 local lspconfig = require('lspconfig')
 
 lspconfig.util.default_config.capabilities = vim.tbl_deep_extend(
-  'force',
-  lspconfig.util.default_config.capabilities,
-  require('cmp_nvim_lsp').default_capabilities()
+    'force',
+    lspconfig.util.default_config.capabilities,
+    require('cmp_nvim_lsp').default_capabilities()
 )
 
 -- Callback function to bind keys for when lsp is attached
@@ -15,7 +15,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set("n", "gi", function() vim.lsp.buf.declaration() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float({source = true}) end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float({ source = true }) end, opts)
         vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
         vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
         vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
@@ -125,7 +125,7 @@ local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 cmp.setup({
     sources = {
-        {name = 'nvim_lsp'},
+        { name = 'nvim_lsp' },
     },
     snippet = {
         expand = function(args)
