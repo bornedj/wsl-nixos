9e0a82e8843ca4408e3ccb2480f63efc3085c817
Daniel Borne <daniel.borne@kinsaleins.com>
2026-06-12 08:51:24 -0400

    fix: lua ls with nvim support

--- home/users/work-wsl/programs/nvim/plugins/lsp.lua
+++ home/users/work-wsl/programs/nvim/plugins/lsp.lua
@@ -48,7 +48,46 @@ vim.lsp.enable('rust_analyzer', {
         }
     }
 })
-vim.lsp.enable('lua_ls', {})
+vim.lsp.config('lua_ls', {
+    on_init = function(client)
+        if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if
+                path ~= vim.fn.stdpath('config')
+                and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+            then
+                return
+            end
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most
+                -- likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- Tell the language server how to find Lua modules same way as Neovim
+                -- (see `:h lua-module-load`)
+                path = {
+                    'lua/?.lua',
+                    'lua/?/init.lua',
+                },
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                    -- For LSP Settings Type Annotations: https://github.com/neovim/nvim-lspconfig#lsp-settings-type-annotations
+                    vim.api.nvim_get_runtime_file("lua/lspconfig", false)[1],
+                },
+            },
+        })
+    end,
+    settings = {
+        Lua = {},
+    },
+})
+vim.lsp.enable('lua_ls')
 vim.lsp.enable('angularls', {})
 vim.lsp.enable('cssls', {})
 vim.lsp.config('emmet_ls', {
@@ -93,7 +132,7 @@ vim.lsp.config('jdtls', {
     init_options = {
         bundles = {
             vim.fn.glob(
-            '~/.local/lib/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar'),
+                '~/.local/lib/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar'),
         }
     },
     settings = {
