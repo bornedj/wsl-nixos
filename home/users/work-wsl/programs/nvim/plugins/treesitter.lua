1d18ca6c3f975524719dc81cb221dac9f13396f0
Daniel Borne <daniel.borne@kinsaleins.com>
2026-06-12 11:06:31 -0400

    fix: proper sytnax highlighting config for treesitter

--- home/users/work-wsl/programs/nvim/plugins/treesitter.lua
+++ home/users/work-wsl/programs/nvim/plugins/treesitter.lua
@@ -1,13 +1,14 @@
-require('nvim-treesitter').setup({
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
-})
+require('nvim-treesitter').setup()
 
-vim.treesitter.language.register("markdown", "mdx")
-vim.treesitter.language.register("bash", "sh")
-vim.treesitter.language.register("java", "java")
+vim.treesitter.language.register('bash', 'sh')
+vim.treesitter.language.register('markdown', 'mdx')
+
+-- turns on the highlighting on FileType completion
+vim.api.nvim_create_autocmd('FileType', {
+    callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+    end,
+})
 
 vim.filetype.add({
     extension = {
