ba44182796e1fc8888398026daa2ba3652fbe972
Daniel Borne <daniel.borne@kinsaleins.com>
2026-06-12 10:46:13 -0400

    fix: rely on withAllGrammars for nvim treesitter install

--- home/users/work-wsl/programs/nvim/plugins/treesitter.lua
+++ home/users/work-wsl/programs/nvim/plugins/treesitter.lua
@@ -1,28 +1,9 @@
--- current workaround to install grammers with nix
--- Defines a read-write directory for treesitters in nvim's cache dir
-local parser_install_dir = vim.fn.stdpath("cache") .. "/treesitters"
-vim.fn.mkdir(parser_install_dir, "p")
--- Prevents reinstall of treesitter plugins every boot
-vim.opt.runtimepath:append(parser_install_dir)
-
 require('nvim-treesitter').setup({
-    auto_install = true,
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
     },
-    install_dir = parser_install_dir,
 })
-
-require('nvim-treesitter').install {
-    "javascript",
-    "typescript",
-    "rust",
-    "lua",
-    "nix",
-    "bash",
-    "java",
-}
 
 local ft_to_parser = require("nvim-treesitter.parsers")
 ft_to_parser.mdx = "markdown"
