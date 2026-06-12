4476f1d65a5c2f10cbc98b3004f9756f8b3f714a
Daniel Borne <daniel.borne@kinsaleins.com>
2026-06-12 08:52:46 -0400

    build: flake update and new channel

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "NixOS configuration";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixos-wsl= {
         url = "github:nix-community/NixOS-WSL/main";
         inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager= {
         url = "github:nix-community/home-manager";
         inputs.nixpkgs.follows = "nixpkgs";
