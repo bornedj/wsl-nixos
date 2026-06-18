f729a18b54c2f3b5786b8cf7db7364a18e5dc328
Daniel Borne <daniel.borne@kinsaleins.com>
2026-06-18 17:00:11 -0400

    refactor: compose db urls

--- home/users/work-wsl/default.nix
+++ home/users/work-wsl/default.nix
@@ -123,9 +123,30 @@
 
         secrets = {
             NPM_PUBLISH_TOKEN.path = "${config.sops.defaultSymlinkPath}/NPM_PUBLISH_TOKEN";
-            POSTGRES_DEV_JDBC_ERD_URL = {
-                key = "DATABASE/POSTGRES/DEV/JDBC_ERD_USER_PASS_URL";
-                path = "${config.sops.defaultSymlinkPath}/POSTGRES_DEV_JDBC_ERD_URL";
+            POSTGRES_DEV_HOST = {
+                key = "DATABASE/POSTGRES/DEV/HOST";
+                path = "${config.sops.defaultSymlinkPath}/POSTGRES_DEV_HOST";
+            };
+            POSTGRES_QA_HOST = {
+                key = "DATABASE/POSTGRES/QA/HOST";
+                path = "${config.sops.defaultSymlinkPath}/POSTGRES_QA_HOST";
+            };
+            POSTGRES_STAGE_HOST = {
+                key = "DATABASE/POSTGRES/STAGE/HOST";
+                path = "${config.sops.defaultSymlinkPath}/POSTGRES_STAGE_HOST";
+            };
+            POSTGRES_PROD_HOST = {
+                key = "DATABASE/POSTGRES/PROD/HOST";
+                path = "${config.sops.defaultSymlinkPath}/POSTGRES_PROD_HOST";
+            };
+
+            POSTGRES_USERNAME = {
+                key = "DATABASE/POSTGRES/USERNAME";
+                path = "${config.sops.defaultSymlinkPath}/POSTGRES_USERNAME";
+            };
+            POSTGRES_PASSWORD = {
+                key = "DATABASE/POSTGRES/PASSWORD";
+                path = "${config.sops.defaultSymlinkPath}/POSTGRES_PASSWORD";
             };
         };
     };
--- home/users/work-wsl/programs/dbeaver.nix
+++ home/users/work-wsl/programs/dbeaver.nix
@@ -1,5 +1,9 @@
 { config, ... }:
 
+let 
+    erdSchema = "dbktentref";
+    corrSchema = "dbktentcorr";
+in
 {
     programs.dbeaver.enable = true;
 
@@ -7,14 +11,58 @@
         path = "${config.xdg.dataHome}/DBeaverData/workspace6/General/.dbeaver/data-sources.json";
         content = builtins.toJSON {
             folders = { };
-            connections = {
+            connections = { 
+                # postgres
+                # erd dallas connections
                 postgres-dev-erd = {
                     provider = "postgresql";
                     driver = "postgres-jdbc";
                     name = "Postgres Dev ERD";
                     save-password = true;
                     configuration = {
-                        url = config.sops.placeholder.POSTGRES_DEV_JDBC_ERD_URL;
+                        url = "jdbc:postgresql://${config.sops.placeholder.POSTGRES_DEV_HOST}${erdSchema}?user=${config.sops.placeholder.POSTGRES_USERNAME}&password=${config.sops.placeholder.POSTGRES_PASSWORD}";
+                        configurationType = "URL";
+                    };
+                };
+                postgres-qa-erd = {
+                    provider = "postgresql";
+                    driver = "postgres-jdbc";
+                    name = "Postgres QA ERD";
+                    save-password = true;
+                    configuration = {
+                        url = "jdbc:postgresql://${config.sops.placeholder.POSTGRES_QA_HOST}${erdSchema}?user=${config.sops.placeholder.POSTGRES_USERNAME}&password=${config.sops.placeholder.POSTGRES_PASSWORD}";
+                        configurationType = "URL";
+                    };
+                };
+                postgres-stage-erd = {
+                    provider = "postgresql";
+                    driver = "postgres-jdbc";
+                    name = "Postgres Stage ERD";
+                    save-password = true;
+                    configuration = {
+                        url = "jdbc:postgresql://${config.sops.placeholder.POSTGRES_STAGE_HOST}${erdSchema}?user=${config.sops.placeholder.POSTGRES_USERNAME}&password=${config.sops.placeholder.POSTGRES_PASSWORD}";
+                        configurationType = "URL";
+                    };
+                };
+                postgres-prod-erd = {
+                    provider = "postgresql";
+                    driver = "postgres-jdbc";
+                    name = "Postgres PROD ERD";
+                    save-password = true;
+                    configuration = {
+                        url = "jdbc:postgresql://${config.sops.placeholder.POSTGRES_PROD_HOST}${erdSchema}?user=${config.sops.placeholder.POSTGRES_USERNAME}&password=${config.sops.placeholder.POSTGRES_PASSWORD}";
+                        configurationType = "URL";
+                    };
+                };
+
+                # correspondence
+                postgres-dev-corr = {
+                    provider = "postgresql";
+                    driver = "postgres-jdbc";
+                    name = "Postgres Dev Correspondence";
+                    save-password = true;
+                    configuration = {
+                        url = "jdbc:postgresql://${config.sops.placeholder.POSTGRES_DEV_HOST}${corrSchema}?user=${config.sops.placeholder.POSTGRES_USERNAME}&password=${config.sops.placeholder.POSTGRES_PASSWORD}";
                         configurationType = "URL";
                     };
                 };
