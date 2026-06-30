39479649043195da2bfda0f25cd330c471ff07fb
Daniel Borne <daniel.borne@kinsaleins.com>
2026-06-30 14:49:29 -0400

    chore: add jira account id

--- home/users/work-wsl/shells/dms.nix
+++ home/users/work-wsl/shells/dms.nix
@@ -33,6 +33,7 @@ pkgs.mkShellNoCC {
     # xray setup
     export XRAY_CLIENT_ID="$(sops decrypt /home/nixos/dotfiles/home/secrets/kinsale.yaml | yq '.XRAY.CLIENT_ID' | tr -d '"')"
     export XRAY_CLIENT_SECRET="$(sops decrypt /home/nixos/dotfiles/home/secrets/kinsale.yaml | yq '.XRAY.CLIENT_SECRET' | tr -d '"')"
+    export JIRA_ACCOUNT_ID="$(sops decrypt /home/nixos/dotfiles/home/secrets/kinsale.yaml | yq '.XRAY.PERSONAL_ACCOUNT_ID' | tr -d '"')"
 
     alias rm_s3_dev="aws s3 rm s3://transit-file-cabinet-scanned-us-east-1-dev/submission/99999999/ --recursive"
     alias dms_run_dev="mvn -Dspring.datasource.secondary.username=$dms_legacy_db_username -Dspring.datasource.secondary.password=$dms_legacy_db_password -Dspring.datasource.primary.password=$dms_db_password -Dspring.datasource.primary.username=$dms_db_username -Dapplication.submission.file-cabinet.username=$smb_sa_username -Dapplication.submission.file-cabinet.password=$smb_sa_password -Dtenantid=$tenantid -P dev --log-file run-logs.log"
