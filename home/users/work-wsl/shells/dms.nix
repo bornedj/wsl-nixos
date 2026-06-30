02466bd49e1c627a914a735624622a92ccfb8374
Daniel Borne <daniel.borne@kinsaleins.com>
2026-06-30 13:48:36 -0400

    chore: add xray secrets to dms shell

--- home/users/work-wsl/shells/dms.nix
+++ home/users/work-wsl/shells/dms.nix
@@ -30,6 +30,10 @@ pkgs.mkShellNoCC {
     export DBKTDOCMGMT_CREDENTIALS_USERNAME="$(sops decrypt /home/nixos/dotfiles/home/secrets/kinsale.yaml | yq '.AUTOMATED_PIPELINE_TESTING.DATABASE.DBKTDOCMGMT.USERNAME' | tr -d '"')"
     export DBKTDOCMGMT_CREDENTIALS_PASSWORD="$(sops decrypt /home/nixos/dotfiles/home/secrets/kinsale.yaml | yq '.AUTOMATED_PIPELINE_TESTING.DATABASE.DBKTDOCMGMT.PASSWORD' | tr -d '"')"
 
+    # xray setup
+    export XRAY_CLIENT_ID="$(sops decrypt /home/nixos/dotfiles/home/secrets/kinsale.yaml | yq '.XRAY.CLIENT_ID' | tr -d '"')"
+    export XRAY_CLIENT_SECRET="$(sops decrypt /home/nixos/dotfiles/home/secrets/kinsale.yaml | yq '.XRAY.CLIENT_SECRET' | tr -d '"')"
+
     alias rm_s3_dev="aws s3 rm s3://transit-file-cabinet-scanned-us-east-1-dev/submission/99999999/ --recursive"
     alias dms_run_dev="mvn -Dspring.datasource.secondary.username=$dms_legacy_db_username -Dspring.datasource.secondary.password=$dms_legacy_db_password -Dspring.datasource.primary.password=$dms_db_password -Dspring.datasource.primary.username=$dms_db_username -Dapplication.submission.file-cabinet.username=$smb_sa_username -Dapplication.submission.file-cabinet.password=$smb_sa_password -Dtenantid=$tenantid -P dev --log-file run-logs.log"
     alias dms_run_local="mvn -Dspring.datasource.secondary.username=$dms_legacy_db_username -Dspring.datasource.secondary.password=$dms_legacy_db_password -Dspring.datasource.primary.password=$dms_db_password -Dspring.datasource.primary.username=$dms_db_username -Dapplication.submission.file-cabinet.username=$smb_sa_username -Dapplication.submission.file-cabinet.password=$smb_sa_password -Dtenantid=$tenantid -P local --log-file run-logs.log"
